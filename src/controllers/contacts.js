b4069ccd5334734dfc728f213e84732b0ae3931e
Shade <kulakaa073@gmail.com>
2025-07-31 05:57:08 +0300

    hotfix
    data was passed as an object instead of variables

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -59,11 +59,7 @@ export const createContactController = async (req, res) => {
 
 export const patchContactController = async (req, res, next) => {
   const { contactId } = req.params;
-  const result = await updateContact({
-    contactId,
-    ...req.body,
-    userId: req.user._id,
-  });
+  const result = await updateContact(contactId, req.body, req.user._id);
 
   if (!result) {
     next(createHttpError(404, 'Contact not found'));
@@ -80,7 +76,7 @@ export const patchContactController = async (req, res, next) => {
 export const deleteContactController = async (req, res, next) => {
   const { contactId } = req.params;
 
-  const contact = await deleteContact({ contactId, userId: req.user._id });
+  const contact = await deleteContact(contactId, req.user._id);
 
   if (!contact) {
     next(createHttpError(404, 'Contact not found'));
